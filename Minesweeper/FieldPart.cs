39a8e7feca12eec0fc4f94bb4567ea85b0a6aa3f
Helen Luu <helleskotta@gmail.com>
2017-08-10 10:55:10 +0200

    Ändrat spelplanen till en array

--- Minesweeper/FieldPart.cs
+++ Minesweeper/FieldPart.cs
@@ -9,6 +9,9 @@ namespace Minesweeper
     {
         public bool IsMine { get; set; }
         public int ID { get; set; }
+        public int Width { get; set; }
+        public int Height { get; set; }
+        public int MineCount { get; set; }
 
         //public int MineBeside { get; set; }
 
@@ -16,6 +19,33 @@ namespace Minesweeper
         {
             IsMine = isMine;
             ID = id;
+        }
+
+        public string DrawField()
+        {
+            Random random = new Random();
+            random.Next(0, Width);
+
+            random.Next(0, Height);
+
+            FieldPart[,] field = new FieldPart[Width, Height];
+
+            string elementOnField = "";
+            int counter = 0;
+
+            // Kolumn
+            for (int y = 0; y < Height; y++)
+            {
+                elementOnField += "<tr>";
+
+                // Rad
+                for (int x = 0; x < Width; x++)
+                {
+                    elementOnField += $"<td style=\"border: 1px solid black;\" id=\"{counter}\" onclick=\"tdclick({counter++}, {field[x, y].IsMine});\"></td>";
+                }
+                elementOnField += "</tr>";
+            }
+            return elementOnField;
         }
     }
 }
--- Minesweeper/index.aspx.cs
+++ Minesweeper/index.aspx.cs
@@ -11,54 +11,50 @@ namespace Minesweeper
     {
         protected void Page_Load(object sender, EventArgs e)
         {
-            // Spelplansrandom
-            Random random = new Random();
-            random.Next(1, 10);
 
-            List<FieldPart> field = new List<FieldPart>();
 
             List<int> playField = new List<int>()
             {
                 3, 4, 5, 6, 7, 8, 9, 10, 11
             };
 
-            foreach (int squareOnField in playField)
-            {
-                int tempID = squareOnField;
-                bool tempMine;
+            //foreach (int squareOnField in playField)
+            //{
+            //    int tempID = squareOnField;
+            //    bool tempMine;
                 
-                // Mine or not
-                if (random.Next(1, 3) == 1)
-                {
-                    tempMine = true;
-                }
-                else
-                {
-                    tempMine = false;
-                }
+            //    // Mine or not
+            //    if (random.Next(1, 3) == 1)
+            //    {
+            //        tempMine = true;
+            //    }
+            //    else
+            //    {
+            //        tempMine = false;
+            //    }
 
-                field.Add(new FieldPart(tempMine, tempID));
-            }
+            //    field.Add(new FieldPart(tempMine, tempID));
+            //}
 
-            string elementOnField = "";
-            int counter = 0;
+            //string elementOnField = "";
+            //int counter = 0;
             Random mine = new Random();
             mine.Next(0, 3);
 
 
-            foreach (var square in field)
-            {
-                if (square.ID % 3 == 0)
-                {
-                    elementOnField += "<tr>";
-                    elementOnField += $"<td style=\"border: 1px solid black;\" class=\"{square.IsMine}\" id=\"{counter}\" onclick=\"tdclick({counter++}, {square.IsMine});\"></td>";
-                    elementOnField += $"<td style=\"border: 1px solid black;\"class=\"{square.IsMine}\" id=\"{counter}\" onclick=\"tdclick({counter++}, {square.IsMine});\"></td>";
-                    elementOnField += $"<td style=\"border: 1px solid black;\"class=\"{square.IsMine}\" id=\"{counter}\" onclick=\"tdclick({counter++}, {square.IsMine});\"></td>";
-                    elementOnField += "</tr>";
-                }
-            }
+            //foreach (var square in field)
+            //{
+            //    if (square.ID % 3 == 0)
+            //    {
+            //        elementOnField += "<tr>";
+            //        elementOnField += $"<td style=\"border: 1px solid black;\" class=\"{(square.IsMine)}\" id=\"{counter}\" onclick=\"tdclick({counter++}, {square.IsMine});\"></td>";
+            //        elementOnField += $"<td style=\"border: 1px solid black;\"class=\"{square.IsMine}\" id=\"{counter}\" onclick=\"tdclick({counter++}, {square.IsMine});\"></td>";
+            //        elementOnField += $"<td style=\"border: 1px solid black;\"class=\"{square.IsMine}\" id=\"{counter}\" onclick=\"tdclick({counter++}, {square.IsMine});\"></td>";
+            //        elementOnField += "</tr>";
+            //    }
+            //}
 
-            PlayField.Text = elementOnField;
+            PlayField.Text = ;
         }
     }
 }
