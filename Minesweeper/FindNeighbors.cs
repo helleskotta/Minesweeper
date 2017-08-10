03968ec7280f9e0c754dd3e11ea454958670ef8b
wibo <william.boquist@gmail.com>
2017-08-10 13:43:45 +0200

    WilliamTest

---
+++ Minesweeper/FindNeighbors.cs
@@ -0,0 +1,19 @@
+﻿//using System;
+//using System.Collections.Generic;
+//using System.Linq;
+//using System.Web;
+
+//namespace Minesweeper
+//{
+//    public class FindNeighbors
+//    {
+//        public FieldPart[,] GetNeighbors(int width, int height, bool isMine)
+//        {
+//            var nearbyPanels = Panels.Where(panel => panel.X >= (x - depth) && panel.X <= (x + depth)
+//                        && panel.Y >= (y - depth) && panel.Y <= (y + depth));
+//            var currentPanel = Panels.Where(panel => panel.X == x && panel.Y == y);
+
+//            return nearbyPanels.Except(currentPanel).ToList();
+//        }
+//    }
+//}
